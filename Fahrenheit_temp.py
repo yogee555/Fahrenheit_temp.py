4d186d478eef535b1dd5237f98a96055b71051f2
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-18 18:04:28 +0530

    Add files via upload

---
+++ Fahrenheit_temp.py
@@ -0,0 +1,4 @@
+# formula method [F=9/5c + 32]  temperature
+celsius = float(input("Enter the celsius temperature"))
+Fahrenheit = 9/5*celsius+32
+print("The equivalent temperature is fahrenhit is",Fahrenheit)
